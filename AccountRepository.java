5135d070777e4eeba5205922929f72eae7f1aee0
AL-FAHAMI TOIHIR <altoihir@gmail.com>
2025-06-28 14:51:58 +0100

    feat: implement repository logic

--- AccountRepository.java
+++ AccountRepository.java
@@ -1,3 +1,24 @@
+import java.util.ArrayList;
+
 public class AccountRepository {
-    
+    private ArrayList<Account> datastore = new ArrayList<>();
+
+    public Boolean createAccount(Account account) {
+        return this.datastore.add(account);
+    }
+
+    public Account retrieveAccount(String accountNumber) {
+        return findAcount(accountNumber);
+    }
+
+    public Boolean removeAccount(String accountNumber) {
+        return datastore.remove(findAcount(accountNumber));
+    }
+
+    private Account findAcount(String accountNumber) {
+        for (Account account : datastore) {
+            if(account.getAccountNumber().equals(accountNumber)) return account;
+        }
+        return null;
+    }
 }
